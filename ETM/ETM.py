c6e7ebb307f63ba7484250260405f8fe2a498526
Blackman White <43486978+BlackmanWhite@users.noreply.github.com>
2018-10-14 11:57:29 -0300

    2.0 Update
    Now you can declare options way easier and get user input directly from the module

--- ETM/ETM.py
+++ ETM/ETM.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
-opt = []
 __author__ = 'Pedro Henrique Santos (Blackman White)'
-__version__ = 1.0
+__version__ = 2.0
 __license__ = 'GNU General Public License v3.0'
+
+opt = []
+index = 1
 
 class Menu():
     def __init__(self, title, character):
@@ -15,23 +17,47 @@ class Menu():
 
     def print_options(self):
         option_n = 1
-        for option in opt:
-            print('[{}] {}'.format(option_n, option))
+        for Option in opt:
+            print('[{}] {}'.format(option_n, Option.option))
             option_n += 1
 
     def print_all(self):
         print('{0}{0}{0}{0}{0}{0}{0}{0}{1}{0}{0}{0}{0}{0}{0}{0}{0}'.format(self.character, self.title))
         option_n = 1
-        for option in opt:
-            print('[{}] {}'.format(option_n, option))
+        for Option in opt:
+            print('[{}] {}'.format(option_n, Option.option))
             option_n += 1
 
-    def add_option(self, option):
-        opt.append(option)
+    def add_option(self, option, command):
+        o = Option(option, command)
+        opt.append(o)
 
-    def print_info(self):
-        print('Title: {}\nOption amount: {}\nOptions: {}'.format(self.title, len(opt), opt))
+    def get_input(self):
+        usr = input('> ')
 
+        for Option in opt:
+            found = False
+            if usr == str(Option.index):
+                found = True
+                Option.command()
+            
+        if found == True:
+            pass
+        
     def about(self):
         print('Made by {}\nVersion: {}\nLicense: {}'.format(__author__,__version__,__license__))
+        
+class Option():
+    def __init__(self, option, command):
+        global index
+        self.option = option
+        self.command = command
+        self.index = index
+        opt.append(self)
+        index += 1
+        
+    def add_option(self, option, command):
+        o = Option(option, command)
+        opt.append(o)
+        print(opt)
         
